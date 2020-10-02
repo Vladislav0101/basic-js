c51a864cc0782b6431eec31c95694dcaa08769a2
Vladislav0101 <by.vlad01@mail.ru>
2020-10-02 14:25:10 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,13 +3,18 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
     let team = new Array();
     for (let i = 0; i < members.length; i++) {
-        if (typeof(members[i]) === 'string') {
-            team[i] = members[i].trim().charAt(0);
+        // if (typeof(members[i]) === 'string') {
+        //     team[i] = members[i].trim().charAt(0);
+        // } else if (typeof(!members[i] === 'string')) {
+        //     members.splice(i, 1);
+        // }
+        if ((typeof(members[i]) === 'string')) {
+            team.push(members[i].trim().charAt(0));
         }
     }
-    if (team == '') {
+    if (!(typeof team[0] === 'string')) {
         return false;
-    };
+    }
     let SortTeam = team.join('').toUpperCase().split('').sort().join('');
     return SortTeam;
 };
