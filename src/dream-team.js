c5f97f345df40c5f5c83ff413eb408342e7fde3e
Vladislav0101 <by.vlad01@mail.ru>
2020-10-03 15:17:14 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,11 +3,6 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
     let team = new Array();
     for (let i = 0; i < members.length; i++) {
-        // if (typeof(members[i]) === 'string') {
-        //     team[i] = members[i].trim().charAt(0);
-        // } else if (typeof(!members[i] === 'string')) {
-        //     members.splice(i, 1);
-        // }
         if ((typeof(members[i]) === 'string')) {
             team.push(members[i].trim().charAt(0));
         }
