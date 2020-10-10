813220f6b78b13ff13d95717d5a0b526e51cce96
Vladislav0101 <by.vlad01@mail.ru>
2020-10-10 23:10:25 +0300

    Update simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -16,6 +16,7 @@ const chainMaker = {
     },
     removeLink(position) {
         if (position <= 0 || typeof position === 'string') {
+            this.chain.splice(0, this.chain.length);
             throw new Error();
         }
         this.chain.splice(position - 1, 1);
@@ -28,6 +29,7 @@ const chainMaker = {
     finishChain() {
         this.str = this.chain.join(' )~~( ');
         this.str = '( ' + this.str + ' )';
+        this.chain.splice(0, this.chain.length);
         return this.str;
     }
 };
