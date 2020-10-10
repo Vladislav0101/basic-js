7612e33c3bb88e3794162df02ee123756bd28de8
Vladislav0101 <by.vlad01@mail.ru>
2020-10-10 23:10:20 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,11 +4,15 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    let count = parseFloat(sampleActivity);
-    const K = 0.693 / HALF_LIFE_PERIOD;
-    if ((typeof sampleActivity !== 'string') || (count < 0 || count > 15) || Number.isNaN(count)) {
-        return false;
+    if (typeof sampleActivity === 'number') {
+        let count = parseFloat(sampleActivity);
+        const K = 0.693 / HALF_LIFE_PERIOD;
+        if ((typeof sampleActivity !== 'string') || (count < 0 || count > 15) || Number.isNaN(count)) {
+            return false;
+        } else {
+            return time = Math.ceil(Math.log(MODERN_ACTIVITY / count) / K);
+        }
     } else {
-        return time = Math.ceil(Math.log(MODERN_ACTIVITY / count) / K);
+        return false;
     }
 };
